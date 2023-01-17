95c01851b990d398b2a071772b4c0b023a9df7ca
inevadim <78503414+inevadim@users.noreply.github.com>
2023-01-17 19:05:43 +0300

    structure toDoList

--- src/components/content/toDoList/ToDoList.jsx
+++ src/components/content/toDoList/ToDoList.jsx
@@ -1,5 +1,12 @@
 import styles from './ToDoList.module.scss';
 
 export const ToDoList = () => {
-  return <div className={styles.toDoList}>toDoList</div>;
+  return (
+    <div className={styles.toDoList}>
+      <div className={styles.toDoListContent}>
+        <div className={styles.name}>toDoList</div>
+        <div className={styles.content}>content</div>
+      </div>
+    </div>
+  );
 };
