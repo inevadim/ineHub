74ddaac1809153a9fe9dadafd21df63b522e44b3
inevadim <78503414+inevadim@users.noreply.github.com>
2023-01-17 18:31:25 +0300

    add style Content

--- src/components/content/Content.jsx
+++ src/components/content/Content.jsx
@@ -1,5 +1,9 @@
 import styles from './Content.module.scss';
 
 export const Content = () => {
-  return <div className={styles.content}></div>;
+  return (
+    <div className={styles.content}>
+      <div className={styles.wrapperContent}></div>
+    </div>
+  );
 };
