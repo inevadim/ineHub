e8bf48624eb3b9159aab5d59cbb87c9260ae5420
inevadim <78503414+inevadim@users.noreply.github.com>
2023-01-17 18:51:18 +0300

    create toDoList

--- src/components/content/Content.jsx
+++ src/components/content/Content.jsx
@@ -1,9 +1,12 @@
 import styles from './Content.module.scss';
+import { ToDoList } from './toDoList/ToDoList';
 
 export const Content = () => {
   return (
     <div className={styles.content}>
-      <div className={styles.wrapperContent}></div>
+      <div className={styles.wrapperContent}>
+        <ToDoList />
+      </div>
     </div>
   );
 };
---
+++ src/components/content/toDoList/ToDoList.jsx
@@ -0,0 +1,5 @@
+import styles from './ToDoList.module.scss';
+
+export const ToDoList = () => {
+  return <div className={styles.toDoList}>toDoList</div>;
+};
