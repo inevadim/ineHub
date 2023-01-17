eccee989b052e8440e6d072d44bcc51992aaad2c
inevadim <78503414+inevadim@users.noreply.github.com>
2023-01-17 19:24:21 +0300

    add redux

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import { Content } from './components/content/Content';
 import { Header } from './components/header/Header';
+import { store } from './redux/store';
+import { Provider } from 'react-redux';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Content />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <Header />
+        <Content />
+      </div>
+    </Provider>
   );
 }
 
--- src/components/content/toDoList/ToDoList.jsx
+++ src/components/content/toDoList/ToDoList.jsx
@@ -1,11 +1,25 @@
 import styles from './ToDoList.module.scss';
 
+import { useSelector, useDispatch } from 'react-redux';
+import { decrement, increment } from '../../../redux/counterSlice';
 export const ToDoList = () => {
+  const count = useSelector(state => state.counter.value);
+  const dispatch = useDispatch();
   return (
     <div className={styles.toDoList}>
       <div className={styles.toDoListContent}>
         <div className={styles.name}>toDoList</div>
-        <div className={styles.content}>content</div>
+        <div className={styles.content}>
+          <div>
+            <button aria-label="Increment value" onClick={() => dispatch(increment())}>
+              Increment
+            </button>
+            <span>{count}</span>
+            <button aria-label="Decrement value" onClick={() => dispatch(decrement())}>
+              Decrement
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
