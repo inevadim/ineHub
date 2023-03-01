e3b8cff3e19f0b736d597e306e452f9965590070
inevadim <78503414+inevadim@users.noreply.github.com>
2023-03-01 22:52:15 +0300

    structure shop

---
+++ src/components/content/shop/shop.jsx
@@ -0,0 +1,9 @@
+import styles from './shop.module.scss';
+
+export const Shop = () => {
+  return (
+    <div className={styles.shop}>
+      <div className={styles.shopContent}>shopContent</div>
+    </div>
+  );
+};
