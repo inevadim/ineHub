653eb060557dd9fc90982b2a8b74aa94055f0428
inevadim <78503414+inevadim@users.noreply.github.com>
2023-03-02 21:43:41 +0300

    data on the bd

--- src/components/content/shop/shop.jsx
+++ src/components/content/shop/shop.jsx
@@ -1,14 +1,25 @@
+import { useEffect, useState } from 'react';
 import { Card } from './components/card/Card';
 import styles from './Shop.module.scss';
 
 export const Shop = () => {
+  const [mas, setMas] = useState([]);
+
+  useEffect(() => {
+    axios.get('/bdShop.json').then(({ data }) => {
+      setMas(data.shop_item);
+    });
+  }, [setMas]);
   return (
     <div className={styles.shop}>
       <div className={styles.shopContent}>
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        {mas.map(item => {
+          return (
+            <div key={item.id}>
+              <Card id={item.id} name={item.name} price={item.price} imgUrl={item.imgUrl} />
+            </div>
+          );
+        })}
       </div>
     </div>
   );
