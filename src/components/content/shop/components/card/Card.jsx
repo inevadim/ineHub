80afdc9dca0481b11f039b95568f960ff48789d6
inevadim <78503414+inevadim@users.noreply.github.com>
2023-03-02 21:32:54 +0300

    added img card

--- src/components/content/shop/components/card/Card.jsx
+++ src/components/content/shop/components/card/Card.jsx
@@ -3,9 +3,12 @@ import styles from './Card.module.scss';
 export const Card = () => {
   return (
     <div className={styles.wrapper}>
-      <div className={styles.imgItem}>img</div>
-      <div className={styles.nameItem}>name</div>
-      <div className={styles.priceItem}>price</div>
+      <div className={styles.imgItem}>
+        <img src="assets/img/shop/jordanOneParis.png" />
+        {/* <img src="assets/img/shop/nikeDunkHighUpWmns.png" /> */}
+      </div>
+      <div className={styles.nameItem}>Jordan One Paris</div>
+      <div className={styles.priceItem}>165 byn</div>
       <div className={styles.buyItem}>buy</div>
     </div>
   );
