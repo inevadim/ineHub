62998e06a04bbc10d75120d358d2cb7f820a4c37
inevadim <78503414+inevadim@users.noreply.github.com>
2023-03-02 21:18:33 +0300

    structure card

--- src/components/content/shop/components/card/Card.jsx
+++ src/components/content/shop/components/card/Card.jsx
@@ -1,5 +1,12 @@
 import styles from './Card.module.scss';
 
 export const Card = () => {
-  return <div className={styles.wrapper}></div>;
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.imgItem}>img</div>
+      <div className={styles.nameItem}>name</div>
+      <div className={styles.priceItem}>price</div>
+      <div className={styles.buyItem}>buy</div>
+    </div>
+  );
 };
