0a5709702f6bda7777baa35c37ca3082163cb3c1
inevadim <78503414+inevadim@users.noreply.github.com>
2023-03-03 15:05:21 +0300

    add routing app

--- src/components/content/Content.jsx
+++ src/components/content/Content.jsx
@@ -1,13 +1,17 @@
 import styles from './Content.module.scss';
 import { Shop } from './shop/Shop';
 import { ToDoList } from './toDoList/ToDoList';
+import { Routes, Route, Link } from 'react-router-dom';
 
 export const Content = () => {
   return (
     <div className={styles.content}>
       <div className={styles.wrapperContent}>
+        <Routes>
+          <Route path="/" element={<ToDoList />} />
+          <Route path="/shop" element={<Shop />} />
+        </Routes>
         {/* <ToDoList /> */}
-        <Shop />
       </div>
     </div>
   );
--- src/components/header/menu/Menu.jsx
+++ src/components/header/menu/Menu.jsx
@@ -1,12 +1,17 @@
 import styles from './Menu.module.scss';
+import { Link } from 'react-router-dom';
 
 export const Menu = () => {
   return (
     <div className={styles.menu}>
       <ul>
         <li>Home</li>
-        <li>To do list</li>
-        <li>Shop</li>
+        <Link to="/">
+          <li>To do list</li>
+        </Link>
+        <Link to="/shop">
+          <li>Shop</li>
+        </Link>
         <li>4</li>
         <li>5</li>
       </ul>
