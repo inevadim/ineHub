cc7dc7b6b946b42552bec58dea89702825ac962a
inevadim <78503414+inevadim@users.noreply.github.com>
2023-03-03 14:25:18 +0300

    styled header shop

--- src/components/content/shop/components/header/Header.jsx
+++ src/components/content/shop/components/header/Header.jsx
@@ -1,5 +1,15 @@
 import styles from './Header.module.scss';
 
 export const Header = () => {
-  return <div className={styles.wrapper}></div>;
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.wrapperName}>
+        <div className={styles.name}>Shop Sneakers</div>
+        <img src="/assets/img/shop/header/sneakers.png" />
+      </div>
+      <div className={styles.shoppingCart}>
+        <img src="/assets/img/shop/header/carts.png" />
+      </div>
+    </div>
+  );
 };
