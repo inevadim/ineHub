1da10dd89971e1b07191d4e7a32528ce67fe8acc
inevadim <78503414+inevadim@users.noreply.github.com>
2023-03-03 14:41:21 +0300

    styled card button

--- src/components/content/shop/components/card/Card.jsx
+++ src/components/content/shop/components/card/Card.jsx
@@ -3,12 +3,14 @@ import styles from './Card.module.scss';
 export const Card = ({ id, name, price, imgUrl }) => {
   return (
     <div className={styles.wrapper}>
-      <div className={styles.imgItem}>
-        <img src={imgUrl} />
-        {/* <img src="assets/img/shop/nikeDunkHighUpWmns.png" /> */}
+      <div className={styles.wrapperContent}>
+        <div className={styles.imgItem}>
+          <img src={imgUrl} />
+          {/* <img src="assets/img/shop/nikeDunkHighUpWmns.png" /> */}
+        </div>
+        <div className={styles.nameItem}>{name}</div>
+        <div className={styles.priceItem}>{price} byn</div>
       </div>
-      <div className={styles.nameItem}>{name}</div>
-      <div className={styles.priceItem}>{price} byn</div>
       <div className={styles.buyItem}>ADD</div>
     </div>
   );
