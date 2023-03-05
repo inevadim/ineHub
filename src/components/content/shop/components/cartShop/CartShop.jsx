ca7cfe2c03b0dcefffaa849f340855985e3b5301
inevadim <78503414+inevadim@users.noreply.github.com>
2023-03-05 16:39:44 +0300

    delete item cart shop

--- src/components/content/shop/components/cartShop/CartShop.jsx
+++ src/components/content/shop/components/cartShop/CartShop.jsx
@@ -1,9 +1,12 @@
 import styles from './CartShop.module.scss';
 import { useDispatch, useSelector } from 'react-redux';
-import { visibleShop } from '../../../../../redux/shopSlice';
+import { visibleShop, deleteItem } from '../../../../../redux/shopSlice';
 export const CartShop = () => {
   const listItems = useSelector(state => state.shop.value);
   const dispatch = useDispatch();
+  const deleteItems = itemName => {
+    return dispatch(deleteItem(itemName));
+  };
   const visibleItem = () => {
     return dispatch(visibleShop());
   };
@@ -22,10 +25,12 @@ export const CartShop = () => {
                 {/* <img src="assets/img/shop/nikeDunkHighUpWmns.png" /> */}
               </div>
               <div className={styles.wrapperPriceName}>
-                <div className={styles.itemName}>name:{item.name}</div>
-                <div className={styles.itemPrice}>price:{item.price}</div>
+                <div className={styles.itemName}>name: {item.name}</div>
+                <div className={styles.itemPrice}>price: {item.price} BYN</div>
               </div>
-              <div className={styles.deleteItem}>⛔️</div>
+              <div className={styles.deleteItem} onClick={() => deleteItems(item.name)}>
+                ⛔️
+              </div>
             </div>
           );
         })}
