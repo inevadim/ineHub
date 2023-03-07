97aba52615aa26be042086d85447bce61d1ed143
inevadim <78503414+inevadim@users.noreply.github.com>
2023-03-07 10:24:24 +0300

    added variables to use state

--- src/components/content/gameClicker/GameClicker.jsx
+++ src/components/content/gameClicker/GameClicker.jsx
@@ -3,20 +3,23 @@ import styles from './GameClicker.module.scss';
 
 export const GameClicker = () => {
   const [count, setCount] = useState(0);
-  const plusCount = () => {
-    setCount(count + 1);
+  const [plusCount, setPlusCount] = useState(1);
+  const [lvlUp, setLvlUp] = useState(10);
+  const plusCountItem = () => {
+    setCount(count + plusCount);
   };
   return (
     <div className={styles.wrapper}>
       <div className={styles.content}>
-        <div className={styles.countGame}>{count}</div>
+        <div className={styles.countGame}>count: {count}</div>
         <div
           className={styles.plusCount}
           onClick={() => {
-            plusCount();
+            plusCountItem();
           }}>
           + count
         </div>
+        <div className={styles.lvlUp}>lvlUp: 10</div>
       </div>
     </div>
   );
