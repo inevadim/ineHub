19ef416899af578d3939b491cb24493ff0d24751
inevadim <78503414+inevadim@users.noreply.github.com>
2023-03-07 10:16:45 +0300

    use usestate and added funcional plus count

--- src/components/content/gameClicker/GameClicker.jsx
+++ src/components/content/gameClicker/GameClicker.jsx
@@ -1,9 +1,23 @@
+import { useState } from 'react';
 import styles from './GameClicker.module.scss';
 
 export const GameClicker = () => {
+  const [count, setCount] = useState(0);
+  const plusCount = () => {
+    setCount(count + 1);
+  };
   return (
     <div className={styles.wrapper}>
-      <div className={styles.content}></div>
+      <div className={styles.content}>
+        <div className={styles.countGame}>{count}</div>
+        <div
+          className={styles.plusCount}
+          onClick={() => {
+            plusCount();
+          }}>
+          + count
+        </div>
+      </div>
     </div>
   );
 };
